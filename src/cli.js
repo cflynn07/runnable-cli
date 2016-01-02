ba585b08a5c3b4d2da0917c1677ab19515db437b
Casey Flynn <cflynn.us@gmail.com>
2016-01-02 00:42:12 -0800

    Seed CLI module file with class outline

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,52 @@
+#!/usr/bin/env node
+
 /**
  * @module bin/cli
  */
+'use strict'
 
-// var program = require('commander');
+var fs = require('fs')
+var program = require('commander')
+
+class CLI {
+  constructor () {
+    var packageJSON = JSON.parse(fs.readFileSync('../package.json'))
+    program.version(packageJSON.version)
+
+    program
+      .command('logs')
+      .action(this._cmdLogs)
+
+    program
+      .command('*')
+      .action(function () { program.help() })
+
+    program.parse(process.argv)
+
+    if (!program.args.length) {
+      program.help()
+    }
+  }
+  _cmdLogs () {
+    console.log('hi!')
+  }
+}
+
+module.exports = new CLI()
+
+/*
+program
+  .command('logs [something]')
+  .description('view logs')
+  .action(function () {
+    console.log('action', arguments)
+  })
+  .command('build-logs', 'view build logs')
+  // .command('terminal', 'terminal', { isDefault: true })
+  .command('status', 'container status')
+  .command('list', 'list containers based on this repository')
+  .command('rebuild', 'rebuild a container')
+  // .command('upload', '')
+  // .command('relaunch')
+  .parse(process.argv)
+*/
