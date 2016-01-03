963ffded6532c24567d5c3f2df446511c5baa8aa
Casey Flynn <cflynn.us@gmail.com>
2016-01-02 18:02:37 -0800

    Seed initial implementation of git module

---
+++ src/git.js
@@ -0,0 +1,52 @@
+/**
+ * @module lib/git
+ * @exports {Function} Git
+ */
+'use strict'
+
+var Promise = require('bluebird')
+var find = require('101/find')
+var hasKeypaths = require('101/has-keypaths')
+var simpleGit = require('simple-git')
+
+class Git {
+  constructor () {
+    this._simpleGit = simpleGit(process.cwd())
+    this._simpleGit.getRemotes = Promise.promisify(this._simpleGit.getRemotes)
+    this._simpleGit.revparse = Promise.promisify(this._simpleGit.revparse)
+  }
+  /**
+   * Fetch repository info of cwd, returns formatted object with selected properties
+   * @param {Function} cb
+   * @returns {Object} Promise, if successful, will resolve with object:
+   *   - branch: {String
+   *   - orgName: {String}
+   *   - repoName: {String}
+   */
+  fetchRepositoryInfo () {
+    var fetchBranch = this._simpleGit.revparse(['--abbrev-ref', 'HEAD'])
+      .then((status) => {
+        return status.replace(/\n$/, '')
+      })
+
+    var fetchRemote = this._simpleGit.getRemotes(true)
+      .then((remotes) => {
+        var originRemote = find(remotes, hasKeypaths({name: 'origin'}))
+        if (!originRemote) {
+          throw new Error('No "origin" remote found for repository')
+        }
+        var matches = originRemote.refs.fetch.match(/:(.+)\/(\D+)\.git/)
+        return {
+          orgName: matches[1],
+          repoName: matches[2]
+        }
+      })
+
+    return Promise.join(fetchBranch, fetchRemote, (branch, remote) => {
+      remote.branch = branch
+      return remote
+    })
+  }
+}
+
+module.exports = Git
