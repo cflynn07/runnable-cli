9a56039ea18c79bdffbc2281c5c6955e2d80da20
Casey Flynn <cflynn.us@gmail.com>
2016-01-04 02:49:23 -0800

    Seed test modules

---
+++ src/cli_test.js
@@ -0,0 +1,196 @@
+#!/usr/bin/env node
+
+/**
+ * @module lib/cli
+ * @exports {Object} instanceOf CLI
+ */
+'use strict'
+
+// Mutates String.prototype globally, only require once in init module
+require('colors')
+
+var program = require('commander')
+
+var ContainerLogs = require('./container-logs')
+var Terminal = require('./terminal')
+var api = require('./api')
+var packageJSON = require('../package.json')
+var status = require('./status')
+
+class CLI {
+  constructor () {
+    program.version(packageJSON.version)
+
+    program
+      .command('logs')
+      .action(this._cmdLogs)
+
+    program
+      .command('terminal')
+      .action(this._cmdTerminal)
+
+    program
+      .command('start')
+      .action(this._cmdStart)
+
+    program
+      .command('stop')
+      .action(this._cmdStop)
+
+    program
+      .command('restart')
+      .action(this._cmdRestart)
+
+    program
+      .command('rebuild')
+      .action(this._cmdRebuild)
+
+    program
+      .command('list')
+      .action(this._cmdList)
+
+    program
+      .command('status')
+      .option('-e', 'Show environment variables')
+      .description('description foo here')
+      .action(this._cmdStatus)
+
+    program
+      .command('browse')
+      .action(this._cmdBrowse)
+
+    program
+      .command('*')
+      .action(function () { program.help() })
+
+    program.parse(process.argv)
+
+    if (!program.args.length) {
+      program.help()
+    }
+  }
+
+  /**
+   * Tail the CMD logs of a container on Runnable.
+   * Attempts to determine which container to use from the origin remote of the current git repo
+   */
+  _cmdLogs () {
+    api.fetchInstance() // TODO: Add org and name args here for manual specification
+      .then((instance) => {
+        var containerLogs = new ContainerLogs(instance.container.dockerHost,
+                                              instance.container.dockerContainer)
+        containerLogs.fetchAndPipeToStdout()
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdTerminal () {
+    api.fetchInstance()
+      .then((instance) => {
+        var terminal = new Terminal(instance.container.dockerHost,
+                                    instance.container.dockerContainer)
+        terminal.fetchAndPipeToStdout()
+      })
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdStart () {
+    api.startInstance()
+      .then((response) => {
+        var output = ['https://runnable.io',
+          response.body.owner.username,
+          response.body.lowerName].join('/') + '\n'
+        output += 'Starting container...'
+        console.log(output.magenta)
+      })
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdStop () {
+    api.stopInstance()
+      .then((response) => {
+        var output = ['https://runnable.io',
+          response.body.owner.username,
+          response.body.lowerName].join('/') + '\n'
+        output += 'Stopping container...'
+        console.log(output.magenta)
+      })
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdRestart () {
+    api.restartInstance()
+      .then((response) => {
+        var output = ['https://runnable.io',
+          response.body.owner.username,
+          response.body.lowerName].join('/') + '\n'
+        output += 'Restarting container...'
+        console.log(output.magenta)
+      })
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdRebuild () {
+    api.rebuildInstance()
+      .then((response) => {
+        var output = ['https://runnable.io',
+          response.body.owner.username,
+          response.body.lowerName].join('/') + '\n'
+        output += 'Rebuilding container...'
+        console.log(output.magenta)
+      })
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+
+  /**
+   *
+   */
+  _cmdList () {
+  }
+
+  /**
+   *
+   */
+  _cmdBrowse () {
+  }
+
+  /**
+   *
+   */
+  _cmdStatus (options) {
+    api.fetchInstance()
+      .then(status.bind(this, options))
+      .catch((err) => {
+        console.log(err.message, err.stack)
+      })
+  }
+}
+
+module.exports = new CLI()
