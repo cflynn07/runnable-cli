a039532e172e5118163fe31d21d9470694271cb4
Casey Flynn <cflynn.us@gmail.com>
2016-01-13 22:07:22 -0800

    Add unit tests for src/output

--- src/status_test.js
+++ src/status_test.js
@@ -2,64 +2,82 @@
  * @module lib/status_test
  */
 
-var test = require('unit.js')
+const test = require('unit.js')
 
-var Status = require('./status')
-var Table = require('./table')
-
-// var instanceMock = require('../test-mocks/instance')
+const Status = require('./status')
+const Table = require('./table')
+const InstanceModel = require('./models/instance')
 
 describe('lib/status.js', () => {
+  var instance
+  var status
+
+  beforeEach(() => {
+    instance = new InstanceModel({})
+    status = new Status(instance)
+  })
+
   it('load', () => {
     test
       .function(Status)
         .hasName('Status')
       .object(new Status())
+        .isInstanceOf(Status)
         .isInstanceOf(Table)
   })
 
   describe('Status class', () => {
     describe('constructor', () => {
-      var status = new Status({})
-      test.object(status.instance).is({})
+      it('should initialize instance properties', () => {
+        test.object(status.instance).is(instance)
+      })
     })
 
     describe('_seedTableData', () => {
+      beforeEach(() => {
+        test.stub(status, '_pushRow')
+        test.stub(instance, 'get')
+      })
+
+      afterEach(() => {
+        status._pushRow.restore()
+        instance.get.restore()
+      })
+
+      it('should push rows with instance data onto table array', () => {
+        status._seedTableData(instance)
+        const instanceStatusKeys = [
+          'branch name',
+          'cid',
+          'created by',
+          'github commit',
+          'open ports',
+          'status',
+          'uptime'
+        ]
+        test.sinon.assert.callCount(instance.get, instanceStatusKeys.length)
+        test.sinon.assert.callCount(status._pushRow, instanceStatusKeys.length)
+        instanceStatusKeys.forEach((val, i) => {
+          test.sinon.assert.calledWith(instance.get.getCall(i, val))
+          test.sinon.assert.calledWith(status._pushRow.getCall(i, val))
+        })
+      })
     })
 
     describe('output', () => {
+      beforeEach(() => {
+        test.stub(status, '_seedTableData')
+      })
+
+      afterEach(() => {
+        status._seedTableData.restore()
+      })
+
+      it('should seed data and call the parent class output method', () => {
+        status.output()
+        test.sinon.assert.calledOnce(status._seedTableData)
+        test.sinon.assert.calledWith(status._seedTableData, instance)
+      })
     })
   })
-/*
-  it('should output formatted table from instance object', (done) => {
-    status({}, instanceMock)
-    sinon.assert.calledTwice(console.log)
-    sinon.assert.calledWith(console.log.firstCall,
-                            'https://runnable.io/CodeNow/websocket-logic-logs-api'.magenta)
-    sinon.assert.calledWith(console.log.secondCall, sinon.match.string)
-    done()
-  })
-
-  it('should output formatted table from instance object if no ports specified', (done) => {
-    var i2 = clone(instanceMock)
-    delete i2.container.ports
-    status({}, i2)
-    sinon.assert.calledTwice(console.log)
-    sinon.assert.calledWith(console.log.firstCall,
-                            'https://runnable.io/CodeNow/websocket-logic-logs-api'.magenta)
-    sinon.assert.calledWith(console.log.secondCall, sinon.match.string)
-    done()
-  })
-
-  it('should output formatted table for instance and instance-envs if E flag spec', (done) => {
-    status({ E: true }, instanceMock)
-    sinon.assert.callCount(console.log, 4)
-    sinon.assert.calledWith(console.log.firstCall,
-                            'https://runnable.io/CodeNow/websocket-logic-logs-api'.magenta)
-    sinon.assert.calledWith(console.log.secondCall, sinon.match.string)
-    sinon.assert.calledWith(console.log.thirdCall, 'ENVIRONMENT VARIABLES'.magenta)
-    sinon.assert.calledWith(console.log.getCall(3), sinon.match.string)
-    done()
-  })
-*/
 })
